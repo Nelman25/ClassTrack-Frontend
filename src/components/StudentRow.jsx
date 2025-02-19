ff34aae5788e3509006c7eb935bea8343bb134bd
Nelman25 <jonelvillaver735@gmail.com>
2025-02-19 18:07:50 +0800

    extract studentRow component

---
+++ src/components/StudentRow.jsx
@@ -0,0 +1,16 @@
+export default function StudentRow({ student }) {
+  const { name, email, studentNumber, course, address } = student;
+  return (
+    <tr className="odd:bg-blue-100 even:bg-white hover:bg-green-100 duration-100 text-lg border-b border-b-slate-300">
+      <td className="py-2 text-start indent-10" colSpan={1}>
+        {name}
+      </td>
+      <td className="text-blue-600" colSpan={3}>
+        {email}
+      </td>
+      <td colSpan={1}>{studentNumber}</td>
+      <td colSpan={1}>{course}</td>
+      <td colSpan={4}>{address}</td>
+    </tr>
+  );
+}
