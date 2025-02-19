1dfe20f983dbadd6d23999d78bf829897660e121
Nelman25 <jonelvillaver735@gmail.com>
2025-02-20 00:48:55 +0800

    install shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        regalBlue: "#34418E",
-        goldenYellow: "#FFD51D",
-        royalIndigo: "#2E2EA1",
-        indigoBlue: "#4040A5",
-        mistGray: "#e4e4e7",
-      },
-    },
+  	extend: {
+  		colors: {
+  			regalBlue: '#34418E',
+  			goldenYellow: '#FFD51D',
+  			royalIndigo: '#2E2EA1',
+  			indigoBlue: '#4040A5',
+  			mistGray: '#e4e4e7'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
