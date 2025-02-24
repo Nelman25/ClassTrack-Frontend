d74b4a47fc257ba98b549f8cefb0f01ad9165cd7
Nelman25 <jonelvillaver735@gmail.com>
2025-02-24 14:59:21 +0800

    add shadcn components

---
+++ src/components/DatePicker.jsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { useState } from "react";
+import { addDays, format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useAttendanceStore } from "@/stores/attendances/attendanceStore";
+
+export function DatePicker({ date }) {
+  const dates = useAttendanceStore((state) => state.dates);
+  const changeDate = useAttendanceStore((state) => state.changeDate);
+  const [dateState, setDateState] = useState(date.attendance_date);
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  }
+
+  console.log(dates);
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[280px] justify-start text-left font-normal",
+            !date && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon />
+          {dateState ? format(dateState, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="flex w-auto flex-col space-y-2 p-2">
+        <Select
+          onValueChange={(value) => {
+            setDateState(addDays(new Date(), parseInt(value)));
+            changeDate(
+              date.date_id,
+              formatDate(addDays(new Date(), parseInt(value)))
+            );
+          }}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="Select" />
+          </SelectTrigger>
+          <SelectContent position="popper">
+            <SelectItem value="0">Today</SelectItem>
+            <SelectItem value="1">Tomorrow</SelectItem>
+            <SelectItem value="3">In 3 days</SelectItem>
+            <SelectItem value="7">In a week</SelectItem>
+          </SelectContent>
+        </Select>
+        <div className="rounded-md border">
+          <Calendar
+            mode="single"
+            selected={dateState}
+            onSelect={(value) => {
+              changeDate(date.date_id, formatDate(value));
+              setDateState(value);
+            }}
+          />
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+}
