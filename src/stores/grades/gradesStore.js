e4455bec2ee0c9b2f31cd61bea34353d87982e73
Nelman25 <jonelvillaver735@gmail.com>
2025-02-25 14:44:28 +0800

    feat: add weight state + add and update weight function

--- src/stores/grades/gradesStore.js
+++ src/stores/grades/gradesStore.js
@@ -6,6 +6,17 @@ export const useGradesStore = create((set) => ({
   error: "",
   gradeTypes: [],
   grades: [],
+  weight: [
+    { gradeType: "Quizzes", percentage: 30 },
+    { gradeType: "Laboratory Activities", percentage: 40 },
+    { gradeType: "Midterm Exam", percentage: 30 },
+  ],
+
+  addWeight: (gradeType, weightValue) => {
+    set((state) => ({ weight: { ...state.weight, [gradeType]: weightValue } }));
+  },
+
+  updateWeight: (updatedWeight) => set({ weight: updatedWeight }),
 
   fetchGradeTypes: async () => {
     set({ loading: true, error: "" });
